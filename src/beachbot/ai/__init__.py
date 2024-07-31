af1e47f18571c0372198c9f3371b7787a4bacc17
Jeffrey Queisser <jeffrey.queisser@oist.jp>
2024-07-31 13:53:31 +0900

    made ssd mobilenet optional, use only if torchvision is available

--- src/beachbot/ai/__init__.py
+++ src/beachbot/ai/__init__.py
@@ -3,7 +3,10 @@ from .debrisdetector import DerbrisDetector
 from .yolov5_onnx import Yolo5Onnx
 from .yolov5_opencv import Yolo5OpenCV
 from .yolov5_torch_hub import Yolo5TorchHub
-from .ssd_mobilenet_torchvision import SSDMobileNetTorchvision
+
+try: from .ssd_mobilenet_torchvision import SSDMobileNetTorchvision
+except: pass
+
 from .dataset import Dataset
 
 
