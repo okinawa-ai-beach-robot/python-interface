2d9922db61c5cc3d38fec69739507ff0db7bd9ea
Jeffrey Queisser <jeffrey.queisser@oist.jp>
2024-07-31 13:52:01 +0900

    added default package version

--- src/beachbot/__init__.py
+++ src/beachbot/__init__.py
@@ -8,10 +8,11 @@ from importlib.metadata import version as _version, PackageNotFoundError
 import logging
 import os, sys
 
+
 try:
     __version__ = _version(__name__)
 except PackageNotFoundError:
-    pass
+    __version__="0.0.0"
 
 
 __all__ = ["manipulators", "sensors", "ai"]  # "sensors", "platforms"
@@ -25,6 +26,20 @@ _handler.setLevel(logging.INFO)
 _formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 _handler.setFormatter(_formatter)
 logger.addHandler(_handler)
+
+def get_base_path():
+    if 'BEACHBOT_HOME' in os.environ:
+        return os.environ.get('BEACHBOT_HOME')
+    else:
+        logger.warning("BEACHBOT_HOME environment variable not specified, fall back to home path " + os.environ['HOME'])
+        return os.environ['HOME']
+
+def get_model_path():
+    return get_base_path() + os.path.sep + "Models"
+def get_dataset_path():
+    return get_base_path() + os.path.sep + "Datasets"
+def get_data_path():
+    return get_base_path() + os.path.sep + "Data"
 
 from . import manipulators
 from . import sensors
