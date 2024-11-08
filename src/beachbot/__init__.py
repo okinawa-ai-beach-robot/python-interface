237e17586ce20193612e934db56eea926a34a2a7
Christopher Buckley <topherbuckley@gmail.com>
2024-11-08 19:10:59 +0900

    wip revert me

--- src/beachbot/__init__.py
+++ src/beachbot/__init__.py
@@ -1,8 +1,5 @@
 from __future__ import annotations
 from importlib.metadata import version as _version, PackageNotFoundError
-import logging
-import os
-import sys
 from . import manipulators
 from . import sensors
 from . import ai
@@ -14,24 +11,3 @@ except PackageNotFoundError:
     __version__ = "0.0.0"
 
 __all__ = ["manipulators", "sensors", "ai", "utils"]  # "sensors", "platforms"
-
-def get_base_path():
-    if "BEACHBOT_HOME" in os.environ:
-        return os.environ.get("BEACHBOT_HOME")
-    else:
-        logger.warning(
-            "BEACHBOT_HOME environment variable not specified, fall back to home path "
-            + os.environ["HOME"]
-        )
-        return os.environ["HOME"]
-
-def get_model_path():
-    return get_base_path() + os.path.sep + "Models"
-
-
-def get_dataset_path():
-    return get_base_path() + os.path.sep + "Datasets"
-
-
-def get_data_path():
-    return get_base_path() + os.path.sep + "Data"
