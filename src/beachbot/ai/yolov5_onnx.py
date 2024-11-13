1e93e3eb0030a64efa9b7deff075f3d5dabfe9e5
Christopher Buckley <topherbuckley@gmail.com>
2024-11-13 12:52:36 +0900

    Non-breaking black auto reformat only to ai/yolov5_onnx.py

--- src/beachbot/ai/yolov5_onnx.py
+++ src/beachbot/ai/yolov5_onnx.py
@@ -4,8 +4,9 @@ from .yolov5_detector import Yolo5Detector
 import onnxruntime
 import numpy as np
 
+
 class Yolo5Onnx(Yolo5Detector):
-    _description="""
+    _description = """
     YOLOv5 implementation based on ONNXruntime framework.\n
     Supports hardware acceleration via CUDA if available on platform.\n
     Tensorrt acceleration is not yet implemented.
@@ -13,12 +14,18 @@ class Yolo5Onnx(Yolo5Detector):
 
     def __init__(self, model_file, use_accel=True) -> None:
         super().__init__(model_file)
-        providers=["CPUExecutionProvider"]
-        if use_accel and 'TensorrtExecutionProvider' in onnxruntime.get_available_providers():
+        providers = ["CPUExecutionProvider"]
+        if (
+            use_accel
+            and "TensorrtExecutionProvider" in onnxruntime.get_available_providers()
+        ):
             logger.info("TODO: Ignore Tensorrt for now!!")
-            providers=["CUDAExecutionProvider"]
-        elif use_accel and 'CUDAExecutionProvider' in onnxruntime.get_available_providers():
-            providers=["CUDAExecutionProvider"]
+            providers = ["CUDAExecutionProvider"]
+        elif (
+            use_accel
+            and "CUDAExecutionProvider" in onnxruntime.get_available_providers()
+        ):
+            providers = ["CUDAExecutionProvider"]
         else:
             logger.info("No Gpu acceleration availabe!")
         logger.info("DL providers are:" + str(providers))
@@ -33,19 +40,23 @@ class Yolo5Onnx(Yolo5Detector):
         self.img_height = input_shapes[2]
         logger.info("model type is " + str(input_type))
         if "float16" in input_type:
-            self.dtype=np.float16
+            self.dtype = np.float16
         elif "float32" in input_type or "float" in input_type:
-            self.dtype=np.float32
+            self.dtype = np.float32
 
-    def apply_model(self, inputs, confidence_threshold=0.2, units_percent=True):  
+    def apply_model(self, inputs, confidence_threshold=0.2, units_percent=True):
         img = self._crop_and_scale_image(inputs)
         img = np.swapaxes(np.swapaxes(img, 0, -1), -2, -1)[None, :, :, :] / 255.0
         if img.dtype != self.dtype:
             img = img.astype(self.dtype)
         prediction = self.session.run(None, {"images": img})
 
-        result_class_ids, result_confidences, result_boxes = self._wrap_detection(prediction[0][0], confidence_threshold=confidence_threshold)
-        self._map_resuts_to_input_image(result_boxes, inputs, units_percent=units_percent)
+        result_class_ids, result_confidences, result_boxes = self._wrap_detection(
+            prediction[0][0], confidence_threshold=confidence_threshold
+        )
+        self._map_resuts_to_input_image(
+            result_boxes, inputs, units_percent=units_percent
+        )
         return result_class_ids, result_confidences, result_boxes
 
 
