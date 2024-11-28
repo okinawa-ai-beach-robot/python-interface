70f14947a840a0501ce417796c5f8119845d8221
Christopher Buckley <topherbuckley@gmail.com>
2024-11-28 10:17:02 +0900

    Removing __all__ and 0.0.0 version naming

--- src/beachbot/__init__.py
+++ src/beachbot/__init__.py
@@ -4,10 +4,3 @@ from . import manipulators
 from . import sensors
 from . import ai
 from . import utils
-
-try:
-    __version__ = _version(__name__)
-except PackageNotFoundError:
-    __version__ = "0.0.0"
-
-__all__ = ["manipulators", "sensors", "ai", "utils"]  # "sensors", "platforms"
