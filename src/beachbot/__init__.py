b7abcaf67735cc91af6d1012a3c4ecd442f13061
Christopher Buckley <topherbuckley@gmail.com>
2024-11-28 13:44:36 +0900

    removing all default imports slowing down imports and showing unrelated errors

--- src/beachbot/__init__.py
+++ src/beachbot/__init__.py
@@ -1,6 +1,2 @@
 from __future__ import annotations
 from importlib.metadata import version as _version, PackageNotFoundError
-from . import manipulators
-from . import sensors
-from . import ai
-from . import utils
