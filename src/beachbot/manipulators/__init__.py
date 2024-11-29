89fcd6c60541ccca452439f48d9dba9a8e8bc2d6
Jeffrey <jeffrey-qeisser@oist.jp>
2024-11-29 14:27:59 +0900

    fixed bug old import

--- src/beachbot/manipulators/__init__.py
+++ src/beachbot/manipulators/__init__.py
@@ -1,5 +1,5 @@
 from .roarmm1 import RoArmM1
-from .drive import Motor, DifferentialDrive
+from .drive import DifferentialDrive
 from .vrepmotorsim import VrepMotorSim
 from .vreproarmm1sim import VrepRoArmM1Sim
 
